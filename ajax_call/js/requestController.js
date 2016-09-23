2150d0af8f026f6695d9676e20704af40d4d25df
yaronlambers <y.lambers@outlook.com>
2016-09-23 14:22:40 +0200

    calls to json

--- ajax_call/js/requestController.js
+++ ajax_call/js/requestController.js
@@ -1,24 +1,32 @@
 $(document).ready(function(){
 
     var list = [];
+    var content = "js/content.json";
+
     function unpack(link){
+        // Items is array
+
         $.get(link, function(data) {
-            var parsed = JSON.parse(data)
+            var parsed = JSON.parse(data);
             var countJson = parsed.length;
-
+            //console.log(countJson);
             for (var i = 0; i < countJson; i++ ){
-                list.push(
-                    parsed[i].title,
-                    parsed[i].content,
-                    parsed[i].img
-                );
-            }
-            console.log(list);
 
-            return list;
+
+                for (i in parsed){
+
+                    var keyOne = Object.keys(parsed[i])[0];//Titles
+                    var keyTwo = Object.keys(parsed[i])[1];//Titles
+                    var keyTree = Object.keys(parsed[i])[2];//img
+
+                    console.log( parsed[i][keyOne] + parsed[i][keyTwo] + parsed[i][keyTree])
+                }
+
+            }
         });
     }
 
-    document.write(unpack("js/content.json"));
+
+    unpack(content);
 
 });
