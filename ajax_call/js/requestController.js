067a313bd0601a538bf20aa238c36fd86caf14ac
yaronlambers <y.lambers@outlook.com>
2016-09-23 16:03:03 +0200

    variable namegiving

--- ajax_call/js/requestController.js
+++ ajax_call/js/requestController.js
@@ -1,27 +1,43 @@
 $(document).ready(function(){
+    // The link is the location of the json file
+    // The divclass is an array where you can deside how you name the div's
+    // The container is where the contend is stored inside the html
 
-    var content = "js/content.json";
+    function unpackContent(link, divClass, container){
 
-    function unpackContent(link, divId){ // amount is how many items in one json block
+        $('body').html($('<div>', {
+            id: container
+        }));
+
         $.get(link, function(data) {
             var parsed = JSON.parse(data);
             var jsonCount = parsed.length;
-            var amount = divId.length;
+            var amount = divClass.length;
 
             for(var i = 0; i < jsonCount; i++ ){
-
                 for(var x = 0; x < amount; x++){
                     var key = Object.keys(parsed[i])[x];
-                    var keyTwo = Object.keys(parsed[0])[0];
 
-                    $("<div class='"+divId[x]+"'/>").html(parsed[i][key]).appendTo("#content");
+                    if (key == "img"){
+                        var img = $('<img />',
+                            {
+                                id: divClass[x],
+                                src: parsed[i][key],
+                            })
+                            .appendTo($('#'+container));
+                    }else{
+                        $("<div class='"+divClass[x]+"'/>").html(parsed[i][key]).appendTo("#"+container);
+                    }
+
                 }
             }
-
         });
     }
 
-    var arr = ["title", "content", "img"];
-    unpackContent(content, arr)
+    var content = "js/content.json";
+    var arr = ["title", "content", "image"];
+    var con = "container";
+
+    unpackContent(content, arr, con)
 
 });
