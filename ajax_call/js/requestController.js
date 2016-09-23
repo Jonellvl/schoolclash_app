1b08ba2e3a9b23b058793ecf01ff6f7a1a6352be
yaronlambers <y.lambers@outlook.com>
2016-09-23 11:22:32 +0200

    working version

--- ajax_call/js/requestController.js
+++ ajax_call/js/requestController.js
@@ -1,14 +1,10 @@
-/**
- * Created by yaronlambers on 22/09/16.
- */
-
 $(document).ready(function(){
 
     var list = [];
     function unpack(link){
         $.get(link, function(data) {
-            parsed = JSON.parse(data)
-            countJson = parsed.length;
+            var parsed = JSON.parse(data)
+            var countJson = parsed.length;
 
             for (var i = 0; i < countJson; i++ ){
                 list.push(
@@ -23,18 +19,6 @@ $(document).ready(function(){
         });
     }
 
-    document.write(unpack("js/content.json").done());
-    //console.log(unpack("js/content.json"));
+    document.write(unpack("js/content.json"));
 
-    //function transferToHtml(content){
-    //    var array = content;
-    //    console.log(content + " array");
-    //    var newHTML = [];
-    //
-    //    $.each(array, function(index, value) {
-    //        newHTML.push('<span>' + value + '</span>');
-    //    });
-    //
-    //    $(".element").html(newHTML.join(""));
-    //}
 });
