5607d0cf051e8fdc77ebb076d9c7f7b7f49cb22b
yaronlambers <y.lambers@outlook.com>
2016-09-26 20:37:16 +0200

    working version

--- ajax_call/js/formBuilder.js
+++ ajax_call/js/formBuilder.js
@@ -3,60 +3,48 @@
  */
 
 //multiForm class form
-$(document).ready(function() {
-    function formBuilder(link, container) {
-        $.get(link, function(data) {
-            $('.form').html($('<div>', {
-                id: container,
-                class: container
-            }));
+function formBuilder(link, container, formId) {
+    $.get(link, function(data) {
+        $('.form').html($('<div>', {
+            id: container,
+            class: container
+        }));
 
-            var parsed = JSON.parse(data);
-            var jsonCount = parsed.length;
-            var amount = 5;
+        var parsed = JSON.parse(data);
+        var jsonCount = parsed.length;
+        var amount = 5;
+        var form = $('<form />',
+            {
+                id: formId
+            })
+            .appendTo($('#'+container));
 
-            var form = $('<form />',
-                {
-                    id: 'quizQuestion'
-                })
-                .appendTo($('#'+container));
-
-            for(var i = 0; i < jsonCount; i++ ){
-                for(var x = 0; x < amount; x++) {
-                    var key = Object.keys(parsed[i])[x];
-                    if (key == "id"){
-                        console.log(parsed[i].id);
+        for(var i = 0; i < jsonCount; i++ ){
+            for(var x = 0; x < amount; x++) {
+                var key = Object.keys(parsed[i])[x];
+                if (key == "id"){
+                    console.log(parsed[i].id);
+                }
+                if (key == "question"){
+                    $( "#"+container ).append( "<h2>"+parsed[i].question+"</h2>" );
+                }
+                if (key == "location"){
+                    $( "#"+container ).append( "<p>"+parsed[i].location+"</p>" );
+                }
+                if (parsed[i][key] == "true"){
+                    var questionCount = parsed[i].answers.answer.length;
+                    $( "#"+formId ).append( "<div class='" + i + "' >" );
+                    for (var q = 0; q < questionCount; q++) {
+                        var question = parsed[i].answers.answer[q];
+                        $('div.' + i).append('<input type="radio" value="' + question + '">' + question + '</input>' + '<br />' );
+                        console.log(question);
                     }
-                    if (key == "question"){
-                        console.log(parsed[i].question);
-                    }
-                    if (key == "location"){
-                        console.log(parsed[i].location);
-                    }
-                    if (parsed[i][key] == "true"){
-                        var questionCount = parsed[i].answers.answer.length;
-                        for (var q = 0; q < questionCount; q++) {
-                            var question = parsed[i].answers.answer[q];
-                            console.log(question);
-                        }
-                    }
-
+                    $( "#"+formId).append( "</div>" );
                 }
             }
-        })
-    }
-
-    var container = 'questions';
-    var link = 'js/question.json';
-    formBuilder(link, container);
-
-});
+        }
+    })
+}
 
 
 
-//var img = $('<img />',
-//    {
-//        class: divClass[x],
-//        src: parsed[i][key],
-//    })
-//    .appendTo($('#'+container));
--- ajax_call/js/requestController.js
+++ ajax_call/js/requestController.js
@@ -1,76 +1,38 @@
-$(document).ready(function(){
+//$(document).ready(function(){
     // The link is the location of the json file
     // The divclass is an array where you can deside how you name the div's
     // The container is where the contend is stored inside the html
     // Bug: cannot give a new container! :(
     // It's necessary to use standard tags for the json/ title, content, img, link
 
-    function unpackContent(link, divClass, container){
-        $('.json').html($('<div>', {
-            id: container
-        }));
+function unpackContent(link, divClass, container){
+    $('.json').html($('<div>', {
+        id: container
+    }));
 
-        $.get(link, function(data) {
-            var parsed = JSON.parse(data);
-            var jsonCount = parsed.length;
-            var amount = divClass.length;
+    $.get(link, function(data) {
+        var parsed = JSON.parse(data);
+        var jsonCount = parsed.length;
+        var amount = divClass.length;
 
-            for(var i = 0; i < jsonCount; i++ ){
-                for(var x = 0; x < amount; x++){
-                    var key = Object.keys(parsed[i])[x];
+        for(var i = 0; i < jsonCount; i++ ){
+            for(var x = 0; x < amount; x++){
 
-                    //switch (key){
-                    //    case key == "img":
-                    //        var img = $('<img />',
-                    //            {
-                    //                class: divClass[x],
-                    //                src: parsed[i][key]
-                    //            })
-                    //            .appendTo($('#'+container));
-                    //        break;
-                    //
-                    //    case key = "link":
-                    //            $("<a>", {
-                    //                title: parsed[i][key],
-                    //                href: parsed[i][key]
-                    //            })
-                    //            .appendTo($('#'+container));
-                    //        break;
-                    //
-                    //    default: {
-                    //        console.log("dit werkt");
-                    //        $("<div class='"+divClass[x]+"'/>").html(parsed[i][key]).appendTo("#"+container);
-                    //    }
-                    //}
+                console.log(key);
+                var key = Object.keys(parsed[i])[x];
 
-                    //if (key == "link"){
-                    //    var link = $("<a>", {
-                    //        title: parsed[i][key],
-                    //        href: parsed[i][key],
-                    //    })
-                    //    .appendTo($('#'+container));
-                    //}
-
-                    if (key == "img"){
-                        var img = $('<img />',
-                            {
-                                class: divClass[x],
-                                src: parsed[i][key],
-                            })
-                            .appendTo($('#'+container));
-                    }else{
-                        $("<div class='"+divClass[x]+"'/>").html(parsed[i][key]).appendTo("#"+container);
-                    }
-
+                if (key == "img"){
+                    var img = $('<img />',
+                        {
+                            class: divClass[x],
+                            src: parsed[i][key]
+                        })
+                        .appendTo($('#'+container));
+                }else{
+                    $("<div class='"+divClass[x]+"'/>").html(parsed[i][key]).appendTo("#"+container);
                 }
+
             }
-        });
-    }
-
-    var content = "js/content.json";
-    var arr = ["title", "content", "image", "link"];
-    var con = "container";
-
-    //unpackContent(content, arr, con
-
-});
+        }
+    });
+}
