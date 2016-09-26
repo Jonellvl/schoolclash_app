941f6d5e9e6fa7359d5f2ee6444da52228b8c55f
yaronlambers <y.lambers@outlook.com>
2016-09-26 14:53:52 +0200

    formbuilder

--- ajax_call/js/formBuilder.js
+++ ajax_call/js/formBuilder.js
@@ -24,15 +24,23 @@ $(document).ready(function() {
             for(var i = 0; i < jsonCount; i++ ){
                 for(var x = 0; x < amount; x++) {
                     var key = Object.keys(parsed[i])[x];
-                    if (parsed[i][key] == "true"){
-                        var questionCount = parsed[i].questions.answer.length;
-
-                        for (var q = 0; q < questionCount; q++){
-                            var question = parsed[i].questions.answer[q];
-
-                        }
-
+                    if (key == "id"){
+                        console.log(parsed[i].id);
                     }
+                    if (key == "question"){
+                        console.log(parsed[i].question);
+                    }
+                    if (key == "location"){
+                        console.log(parsed[i].location);
+                    }
+                    if (parsed[i][key] == "true"){
+                        var questionCount = parsed[i].answers.answer.length;
+                        for (var q = 0; q < questionCount; q++) {
+                            var question = parsed[i].answers.answer[q];
+                            console.log(question);
+                        }
+                    }
+
                 }
             }
         })
