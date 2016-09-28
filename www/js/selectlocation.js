c1bfab3a7218d8680a05dc5527ff13f59c122db0
ddnetters <d.d.netters@gmail.com>
2016-09-28 15:14:42 +0200

    added ajax get request

--- www/js/selectlocation.js
+++ www/js/selectlocation.js
@@ -1,15 +1,9 @@
 function getLocations(){
 	var url = "http://api.schoolclash.eu:5000/api/location";
-
-$.getJSON( url, function( data ) {
-  var items = [];
-  $.each( data, function( key, val ) {
-    items.push( "<li id='" + key + "'>" + val + "</li>" );
-  });
- 
-  $( "<ul/>", {
-    "class": "my-new-list",
-    html: items.join( "" )
-  }).appendTo( "body" );
-});
+	console.log(1);
+	
+	$.getJSON(url, function(data) {
+	  alert(data);
+	  console.log(2);
+	});
 }
