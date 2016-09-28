4bbc235e7bfced47f085c25d885d6fd00792e975
yaronlambers <y.lambers@outlook.com>
2016-09-28 16:29:53 +0200

    created header function

--- www/js/header.js
+++ www/js/header.js
@@ -1,18 +1,23 @@
 /**
  * Created by yaronlambers on 28/09/16.
  */
-$( document ).ready(function(){
+function header(cssLink){
 
-    var $head = $("head");
-    var $headlinklast = $head.find("link[rel='stylesheet']:last");
-    var linkElement = "<link rel='stylesheet' href='/css/masterBlaster.css' type='text/css' media='screen'>";
+    var css = "css/"+cssLink;
+    console.log(css);
+
+    var head = $("head");
+    var headlinklast = head.find("link[rel='stylesheet']:last");
+    var linkElement = "<link rel='stylesheet' href='/css/fietsen.css' type='text/css' media='screen'>";
+    //var linkElement = "<link rel='stylesheet' href='/css/fietsen.css' type='text/css' media='screen'>";
     if ($headlinklast.length){
         $headlinklast.after(linkElement);
     }
     else {
         $head.append(linkElement);
     }
+}
 
-});
+
 
 
