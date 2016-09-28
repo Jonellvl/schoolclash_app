f66c7ba660966be802dadd751b46ae55b10eb8f8
jelmerbou <jelmer_bouman@hotmail.com>
2016-09-28 12:26:30 +0200

    hallo

--- platforms/android/assets/www/js/header.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by yaronlambers on 28/09/16.
- */
-
-var script=document.createElement('script');
-script.type='text/javascript';
-script.src='js/jquery-3.1.1.js';
-
-$("head").append(script);
