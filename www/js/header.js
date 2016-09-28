df63976f0ccbc7e928a5b0345e401a9a04d08c22
yaronlambers <y.lambers@outlook.com>
2016-09-28 16:50:50 +0200

    wip

--- www/js/header.js
+++ www/js/header.js
@@ -2,19 +2,17 @@
  * Created by yaronlambers on 28/09/16.
  */
 function header(cssLink){
-
-    var css = "css/"+cssLink;
-    console.log(css);
-
-    var head = $("head");
-    var headlinklast = head.find("link[rel='stylesheet']:last");
-    var linkElement = "<link rel='stylesheet' href='/css/fietsen.css' type='text/css' media='screen'>";
-    //var linkElement = "<link rel='stylesheet' href='/css/fietsen.css' type='text/css' media='screen'>";
-    if ($headlinklast.length){
-        $headlinklast.after(linkElement);
-    }
-    else {
-        $head.append(linkElement);
+    var countCss = cssLink.length;
+    for (var i = 0; i < countCss; i++){
+        var head = $("head");
+        var headlinklast = head.find("link[rel='stylesheet']:last");
+        var linkElement = "<link rel='stylesheet' href='" +  cssLink[i] +"' type='text/css' media='screen'>";
+        if (headlinklast.length){
+            headlinklast.after(linkElement);
+        }
+        else {
+            head.append(linkElement);
+        }
     }
 }
 
