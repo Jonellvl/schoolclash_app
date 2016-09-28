5b06a4abf7ae0b5b4f68249ac3485fb78ac01a8c
ddnetters <d.d.netters@gmail.com>
2016-09-28 15:10:08 +0200

    wip

---
+++ www/js/selectlocation.js
@@ -0,0 +1,15 @@
+function getLocations(){
+	var url = "http://api.schoolclash.eu:5000/api/location";
+
+$.getJSON( url, function( data ) {
+  var items = [];
+  $.each( data, function( key, val ) {
+    items.push( "<li id='" + key + "'>" + val + "</li>" );
+  });
+ 
+  $( "<ul/>", {
+    "class": "my-new-list",
+    html: items.join( "" )
+  }).appendTo( "body" );
+});
+}
