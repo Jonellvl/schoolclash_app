5647385156cd5cd9cffbb0831a4063331585daa4
yaron lambers <yaronlambers@yarons-MBP.fritz.box>
2016-09-29 13:56:28 +0200

    wip

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -517,14 +517,6 @@ module.exports = [
         "clobbers": [
             "CameraPopoverHandle"
         ]
-    },
-    {
-        "file": "plugins/cordova-sqlite-storage/www/SQLitePlugin.js",
-        "id": "cordova-sqlite-storage.SQLitePlugin",
-        "pluginId": "cordova-sqlite-storage",
-        "clobbers": [
-            "SQLitePlugin"
-        ]
     }
 ];
 module.exports.metadata = 
@@ -550,8 +542,7 @@ module.exports.metadata =
     "cordova-plugin-statusbar": "2.1.3",
     "cordova-plugin-vibration": "2.1.2",
     "cordova-plugin-whitelist": "1.2.2",
-    "org.apache.cordova.camera": "0.3.5",
-    "cordova-sqlite-storage": "1.4.7"
-}
+    "org.apache.cordova.camera": "0.3.5"
+};
 // BOTTOM OF METADATA
 });
