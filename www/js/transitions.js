1034654e8cafe16e2b6b4f5d663e26eec5fbadbb
Jonell <jonell98@hotmail.com>
2016-09-30 10:42:58 +0200

    Adding transitions

---
+++ www/js/transitions.js
@@ -0,0 +1,11 @@
+function fade_in() {
+  $('body').fadeIn(300);
+}
+
+function fade_out() {
+  $('body').fadeOut(400);
+}
+
+$("a").click(function() {
+  alert( "Handler for .click() called." );
+});
