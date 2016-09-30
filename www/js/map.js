46c9cfc3d3fe085d82383cfa3bdd1eccc0c69fb1
ddnetters <d.d.netters@gmail.com>
2016-09-30 10:27:29 +0200

    wip

--- www/js/map.js
+++ www/js/map.js
@@ -61,7 +61,7 @@ var app = {
 
                 for (var i = longLats.length - 1; i >= 0; i--) {
                 	longLats[i];
-                	L.marker([longLats[i][0], longLats[i][1]]).addTo(map).bindPopup(longLats[i][2]).openPopup();
+                	L.marker([longLats[i][1], longLats[i][0]]).addTo(map).bindPopup(longLats[i][2]).openPopup();
                 }
 		var popup = L.popup();
 
