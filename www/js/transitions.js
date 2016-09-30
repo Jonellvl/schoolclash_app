be5f1eaf0d82922afe52efb8200d2053f0e5b3b7
yaron lambers <yaronlambers@yarons-MBP.fritz.box>
2016-09-30 13:29:26 +0200

    Deleted transition

--- www/js/transitions.js
+++ www/js/transitions.js
@@ -1,9 +1,25 @@
-function fade_in() {
-  $('body').fadeIn(300);
-}
-function fade_out() {
-  $('body').fadeOut(400);
-}
-$('.locations_button').click(function() {
-    alert("Testje!");
-});
+//function fade_in() {
+//    $('body').fadeIn(200);
+//    //setTimeout(function(){
+//    //    $('body').css("background-color", "#fff");
+//        $('body').css("transition", "200ms");
+//    //}, 500);
+//
+//
+//}
+//
+//function fade(){
+//    $('header').css("opacity", "1");
+//    $('#location-blocks').css("opacity", "1");
+//    $('.container').css("opacity", "1");
+//    $('.header').css("opacity", "1");
+//}
+
+function fade_in(){}
+
+//function fade_in(){
+//    $('body').fadeIn(200);
+//    setTimeout(function(){
+//        $('body').css("transition", "200ms");
+//    }, 500);
+//}
