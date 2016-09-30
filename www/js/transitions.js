68056b7fd123507584188f6b3cbf6f92ba18ec47
Jonell <jonell98@hotmail.com>
2016-09-30 10:55:52 +0200

    testjes etc

--- www/js/transitions.js
+++ www/js/transitions.js
@@ -1,11 +1,9 @@
 function fade_in() {
   $('body').fadeIn(300);
 }
-
 function fade_out() {
   $('body').fadeOut(400);
 }
-
-$("a").click(function() {
-  alert( "Handler for .click() called." );
+$('.locations_button').click(function() {
+    alert("Testje!");
 });
